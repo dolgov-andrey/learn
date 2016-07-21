10adb11f6c499f37dc426b2f35bf1f5258193b03
Andry <cdolgovc@gmail.ru>
2016-07-21 18:51:33 +0300

    добавляю 2 js для другого задания

--- 1.js
+++ 1.js
@@ -1,10 +1,16 @@
 "use strict";
-var	name = "Василий", 
-	admin = name;
-	
 
-
-alert(admin); // а так проще)
+var	ourPlanet = prompt('введите название планеты'); 
+var	visitorOld = +prompt('введите Свой возвраст');
+var	girlfriendAge;
+ 	
+ 	if (!isNaN(visitorOld)){
+	girlfriendAge = visitorOld/2 +7;
+ 		alert('возвраст допустимой особи обожания '+ girlfriendAge);
+	}else{
+		alert('введите ваш возвраст');
+	}
+ // а так проще)
 /*бла бла 
 проводим тут всякое*/
 
