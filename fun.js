10ff6f8ce62054691086b022383852204c83a1a8
dolgov-andrey <cdolgovc@gmail.com>
2016-07-24 14:53:02 +0300

    Homework
    test

--- fun.js
+++ fun.js
@@ -1,15 +1,60 @@
 'use strict';
+var salaries1= {
+	'Petya': 100,
+	'Vasya': 300,
+	'Dasha': 250
+};
+var currency = '$';
 
-console.log(pow(3,2));
-console.log(pow(3,3));
-console.log(pow(1,100));
-console.log(pow(2,2));
+console.log(sumSalares(salaries1));
+console.log(minSalary(salaries1));
+console.log(maxSalary(salaries1));
 
-function pow(x,n){
-	var res=1;
-	 for (var i = n; i >= 1; i--) {
-		res*= x;
-	}
-	return res;
+function sumSalares(salaries){
+var sum = 0;
+var state = 0;	
+	for (var name in salaries){
+		sum += +salaries[name];
+		state++;
+		} if (0 === state) {
+	return 'к сожалению на вашей фирме официальных сотрудников'+
+		' c зарплатой не найдено';
+		} 
 
+	return 'Штат ваших сотрудников: '+ state +
+		' Общая З/П: '+ sum + currency +
+		' средняя зарплата по фирме: ' +
+		(Math.ceil(sum/state))+ currency;
 }
+
+function minSalary(salaries){
+var res;
+	for(var min in salaries){
+		if (res > +salaries[min]){
+			res = +salaries[min];
+		} 
+	return 'минимальный оклад на вашей фирме составляет: '+
+		res + currency;
+	}
+//		if (0 === res){									// проверка на 0			
+//return	'кажется у вас появились рабы, без З/П '+
+//		res + currency;	
+//		}
+};
+
+
+function maxSalary(salaries){
+var res;
+	for(var max in salaries){
+		if(res <= +salaries[max]){
+			res = +salaries[max]; 
+		} 
+//		if(0 === res) {									// проверка на 0
+//	return 'Если вы считаете что отличная зарплата' +
+//		' это корочка хлеба и стакан теплой воды...' +
+//		' то пора задуматься о премиях';
+//		}
+	return 'Самый большой оклад на вашей фирме составляет:' +
+		res + currency;
+	}
+};
