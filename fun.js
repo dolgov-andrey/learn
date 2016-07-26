25c14b489d967d812e4206fc08fe6da5ba6560c4
Andry <cdolgovc@gmail.ru>
2016-07-26 15:26:23 +0300

    переписаны функции. исправлены синтаксические ошибки в их описании. заменены крайные значения на Infinity & -Infinity. исправлен script Stile  стоит вложить функции в функцию sumSalaries()

--- fun.js
+++ fun.js
@@ -1,60 +1,53 @@
 'use strict';
-var salaries1= {
-	'Petya': 100,
-	'Vasya': 300,
-	'Dasha': 250
+var salaries1 = {
+//	'Petya': 100,
+//	'Vasya': 300,
+//	'Dasha': 250
 };
 var currency = '$';
 
-console.log(sumSalares(salaries1));
-console.log(minSalary(salaries1));
-console.log(maxSalary(salaries1));
+console.log(sumSalaries(salaries1));
+console.log(minSalaries(salaries1));
+console.log(maxSalaries(salaries1));
 
-function sumSalares(salaries){
-var sum = 0;
-var state = 0;	
-	for (var name in salaries){
-		sum += +salaries[name];
-		state++;
-		} if (0 === state) {
-	return 'к сожалению на вашей фирме официальных сотрудников'+
-		' c зарплатой не найдено';
-		} 
+function sumSalaries(salaries){
+  var sum = 0;
+  var state = 0;	
+  var hasWorkers = false;
+  for (var name in salaries){
+	sum += +salaries[name];
+	hasWorkers = true;
+	state++;
+  } if (hasWorkers === false) {
+  return 'к сожалению на вашей фирме официальных сотрудников'+
+		  ' c зарплатой не найдено';
+	} 
 
-	return 'Штат ваших сотрудников: '+ state +
-		' Общая З/П: '+ sum + currency +
-		' средняя зарплата по фирме: ' +
-		(Math.ceil(sum/state))+ currency;
-}
+  return 'Штат ваших сотрудников: '+ state +
+		 ' Общая З/П: '+ sum.toFixed(2) + currency +
+		 ' средняя зарплата по фирме: ' +
+		 ( (sum / state).toFixed(2) )+ currency;
+};
 
-function minSalary(salaries){
-var res;
+function minSalaries(salaries){
+var res = Infinity;
 	for(var min in salaries){
 		if (res > +salaries[min]){
 			res = +salaries[min];
 		} 
 	return 'минимальный оклад на вашей фирме составляет: '+
-		res + currency;
+		res.toFixed(2) + currency;
 	}
-//		if (0 === res){									// проверка на 0			
-//return	'кажется у вас появились рабы, без З/П '+
-//		res + currency;	
-//		}
 };
 
 
-function maxSalary(salaries){
-var res;
+function maxSalaries(salaries){
+var res = -Infinity;
 	for(var max in salaries){
 		if(res <= +salaries[max]){
-			res = +salaries[max]; 
+		  res = +salaries[max]; 
 		} 
-//		if(0 === res) {									// проверка на 0
-//	return 'Если вы считаете что отличная зарплата' +
-//		' это корочка хлеба и стакан теплой воды...' +
-//		' то пора задуматься о премиях';
-//		}
 	return 'Самый большой оклад на вашей фирме составляет:' +
-		res + currency;
+		res.toFixed(2) + currency;
 	}
 };
