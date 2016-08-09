1de91737a98dcc7cd2c59583de90354c3612d6a6
dolgov-andrey <cdolgovc@gmail.com>
2016-08-09 17:38:58 +0300

    * установил конфиг от Яндекса для jscs * установил jscs не конфликтующий с конфигом * авто-реформат кодстайла в программах

--- 1.js
+++ 1.js
@@ -5,10 +5,10 @@ var	visitorOld = +prompt('введите Свой возвраст');
 var	girlfriendAge;
 
 if (!isNaN(visitorOld)) {
-	                                          girlfriendAge = visitorOld / 2 + 7;
- 		                                alert('возвраст допустимой особи обожания ' + girlfriendAge);
+    girlfriendAge = visitorOld / 2 + 7;
+    alert('возвраст допустимой особи обожания ' + girlfriendAge);
 	  } else {
-		                                          alert('введите ваш возвраст');
+    alert('введите ваш возвраст');
 	  }
  // а так проще)
 /*бла бла
--- fun.js
+++ fun.js
@@ -15,13 +15,13 @@ function sumSalaries(salaries) {
     var state = 0;
     var hasWorkers = false;
     for (var name in salaries) {
-	                                                                              sum += +salaries[name];
-	                                                                              hasWorkers = true;
-	                                                                              state++;
+        sum += +salaries[name];
+        hasWorkers = true;
+        state++;
     } if (hasWorkers === false) {
         return 'к сожалению на вашей фирме официальных сотрудников' +
 		  ' c зарплатой не найдено';
-	                                    }
+    }
 
     return 'Штат ваших сотрудников: ' + state +
 		 ' Общая З/П: ' + sum.toFixed(2) + currency +
@@ -31,22 +31,22 @@ function sumSalaries(salaries) {
 
 function minSalaries(salaries) {
     var res = Infinity;
-	                                        for (var min in salaries) {
-		                                        if (res > +salaries[min]) {
-			                                        res = +salaries[min];
-		}
-	                                        return 'минимальный оклад на вашей фирме составляет: ' +
-		res.toFixed(2) + currency;
-	}
+    for (var min in salaries) {
+        if (res > +salaries[min]) {
+            res = +salaries[min];
+        }
+        return 'минимальный оклад на вашей фирме составляет: ' +
+        res.toFixed(2) + currency;
+    }
 }
 
 function maxSalaries(salaries) {
     var res = -Infinity;
-	                                        for (var max in salaries) {
-		                                        if (res <= +salaries[max]) {
-		                                          res = +salaries[max];
-		}
-	                                        return 'Самый большой оклад на вашей фирме составляет:' +
-		res.toFixed(2) + currency;
-	}
+    for (var max in salaries) {
+        if (res <= +salaries[max]) {
+            res = +salaries[max];
+        }
+        return 'Самый большой оклад на вашей фирме составляет:' +
+        res.toFixed(2) + currency;
+    }
 }
