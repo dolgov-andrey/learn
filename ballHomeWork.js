26ee154c8641c7f834173f99b8fe15d6f7cc0a6c
dolgov-andrey <cdolgovc@gmail.com>
2016-08-10 13:53:17 +0300

    Создание тестов в формате *console.log()

--- ballHomeWork.js
+++ ballHomeWork.js
@@ -38,7 +38,6 @@ window.getPositionX = function (startPosX, startSpeedX, curTime){
     distance = startPosX + startSpeedX * curTime;
     return distance.toFixed(2);
 };
-
     /*
     тест на начальные условия по оси 0Y: нач. скорость 0, положение Yo = 0
      */
@@ -46,7 +45,65 @@ console.log(window.getPositionY(0, 0, 100) === 0);
 console.log(window.getPositionY(0, 0, 100));
 
     /*
+     тест свободного падения мячика с высоты Yo != 0
+     */
+console.log(window.getPositionY(5, 0, 7) > 0);
+console.log(window.getPositionY(5, 0, 7));
+    /*
+    тест броска мячика с высоты Yo = 0, startSpeedY =  Vo
+    */
+console.log(window.getPositionY(0, 8, 3) > 0);
+console.log(window.getPositionY(0, 8, 3));
+
+    /*
+     тест броска мячика с высоты Yo = 0, startSpeedY =  - Vo
+     */
+console.log(window.getPositionY(0, -8, 3) > 0);
+console.log(window.getPositionY(0, -8, 3));
+    /*
+     тест броска мячика с высоты Yo != 0, и - Vo
+     */
+console.log(window.getPositionY(7, -8, 3) > 0);
+console.log(window.getPositionY(7, -8, 3));
+    /*
     тетс на начальные условия по оси 0Х: нач. скорость 0. положение Xo = 0
+    мячик движется вертикально
      */
 console.log(window.getPositionX(0, 0, 100) === 0);
 console.log(window.getPositionX(0, 0, 100));
+
+    /*
+    тесты на положительные скорости:
+     Yo = 0
+     Xo = 0
+     угол броска 45deg; startSpeedY * cos(45deg) = startSpeedX * sin(45deg)
+     */
+console.log(window.getPositionY(0, 15, 8) > 0);
+console.log(window.getPositionY(0, 15, 8));
+
+console.log(window.getPositionX(0, 15, 8) > 0);
+console.log(window.getPositionX(0, 15, 8));
+
+    /*
+     тесты на положительные скорости:
+     Yo != 0
+     Xo != 0
+     угол броска 45deg
+     */
+console.log(window.getPositionY(7, 18, 9) > 0);
+console.log(window.getPositionY(7, 18, 9));
+
+console.log(window.getPositionX(3, 18, 9) > 0);
+console.log(window.getPositionX(3, 18, 9));
+
+    /*
+     тесты на отрицательные скорости скорости:
+     Yo != 0
+     Xo != 0
+     угол броска 45deg
+     */
+console.log(window.getPositionY(3, -3, 3) > 0);
+console.log(window.getPositionY(3, -3, 3));
+
+console.log(window.getPositionX(3, 3, 9) > 0);
+console.log(window.getPositionX(3, 3, 9));
