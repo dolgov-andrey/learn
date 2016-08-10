446f6795fc11c721e5a571e6b88af1202a718ec5
dolgov-andrey <cdolgovc@gmail.com>
2016-08-10 13:55:01 +0300

    исправлен синтаксис коментариев

--- ballHomeWork.js
+++ ballHomeWork.js
@@ -29,79 +29,79 @@ window.getPositionY = function (startPosY, startSpeedY, curTime) {
     }
 };
 
-window.getPositionX = function (startPosX, startSpeedX, curTime){
+window.getPositionX = function (startPosX, startSpeedX, curTime) {
     'use strict'
-    if (startPosX === 0 && startSpeedX === 0){
+    if (startPosX === 0 && startSpeedX === 0) {
         return 0;
     }
     var distance = 0;
     distance = startPosX + startSpeedX * curTime;
     return distance.toFixed(2);
 };
-    /*
-    тест на начальные условия по оси 0Y: нач. скорость 0, положение Yo = 0
-     */
+/*
+ тест на начальные условия по оси 0Y: нач. скорость 0, положение Yo = 0
+ */
 console.log(window.getPositionY(0, 0, 100) === 0);
 console.log(window.getPositionY(0, 0, 100));
 
-    /*
-     тест свободного падения мячика с высоты Yo != 0
-     */
+/*
+ тест свободного падения мячика с высоты Yo != 0
+ */
 console.log(window.getPositionY(5, 0, 7) > 0);
 console.log(window.getPositionY(5, 0, 7));
-    /*
-    тест броска мячика с высоты Yo = 0, startSpeedY =  Vo
-    */
+/*
+ тест броска мячика с высоты Yo = 0, startSpeedY =  Vo
+ */
 console.log(window.getPositionY(0, 8, 3) > 0);
 console.log(window.getPositionY(0, 8, 3));
 
-    /*
-     тест броска мячика с высоты Yo = 0, startSpeedY =  - Vo
-     */
+/*
+ тест броска мячика с высоты Yo = 0, startSpeedY =  - Vo
+ */
 console.log(window.getPositionY(0, -8, 3) > 0);
 console.log(window.getPositionY(0, -8, 3));
-    /*
-     тест броска мячика с высоты Yo != 0, и - Vo
-     */
+/*
+ тест броска мячика с высоты Yo != 0, и - Vo
+ */
 console.log(window.getPositionY(7, -8, 3) > 0);
 console.log(window.getPositionY(7, -8, 3));
-    /*
-    тетс на начальные условия по оси 0Х: нач. скорость 0. положение Xo = 0
-    мячик движется вертикально
-     */
+/*
+ тетс на начальные условия по оси 0Х: нач. скорость 0. положение Xo = 0
+ мячик движется вертикально
+ */
 console.log(window.getPositionX(0, 0, 100) === 0);
 console.log(window.getPositionX(0, 0, 100));
 
-    /*
-    тесты на положительные скорости:
-     Yo = 0
-     Xo = 0
-     угол броска 45deg; startSpeedY * cos(45deg) = startSpeedX * sin(45deg)
-     */
+/*
+ тесты на положительные скорости:
+ Yo = 0
+ Xo = 0
+ угол броска 45deg; startSpeedY * cos(45deg) = startSpeedX * sin(45deg)
+ */
 console.log(window.getPositionY(0, 15, 8) > 0);
 console.log(window.getPositionY(0, 15, 8));
 
 console.log(window.getPositionX(0, 15, 8) > 0);
 console.log(window.getPositionX(0, 15, 8));
 
-    /*
-     тесты на положительные скорости:
-     Yo != 0
-     Xo != 0
-     угол броска 45deg
-     */
+/*
+ тесты на положительные скорости:
+ Yo != 0
+ Xo != 0
+ угол броска 45deg
+ */
 console.log(window.getPositionY(7, 18, 9) > 0);
 console.log(window.getPositionY(7, 18, 9));
 
 console.log(window.getPositionX(3, 18, 9) > 0);
 console.log(window.getPositionX(3, 18, 9));
 
-    /*
-     тесты на отрицательные скорости скорости:
-     Yo != 0
-     Xo != 0
-     угол броска 45deg
-     */
+/*
+ тесты на отрицательные скорости скорости:
+ Yo != 0
+ Xo != 0
+ угол броска 45deg
+ */
 console.log(window.getPositionY(3, -3, 3) > 0);
 console.log(window.getPositionY(3, -3, 3));
 
