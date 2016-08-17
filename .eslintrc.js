7e9de2959287681ea5ffef6f8c26adf140e13a4a
Gvozd <gvozdev.viktor@gmail.com>
2016-08-18 00:20:40 +0300

    в процессе перетаскивания потерялись глобальные настройки eslint/jscs так как настройки были только в папке test, то в любых других файлах проверить файлы не удавалось внимательнее читай ошибки которые webstorm пишет вверху файла для jscs он сразу так и пишет - мол потерян конфиш-файл для eslint нужно кликнуть на details, и узнать в чем проблема
    что теперь получилось:
    * в корне проекта есть настройки для eslint и jscs
    по-умолчанию они действуют на абсолютно все js-файлы в проекте
    ВАЖНО: у корневого файла .eslintrc.js должна быть опция root: true, означающая что это корень проекта
    у более глубоких конфигов eslint эта опция наоборот должна отсутствовать(ведь они находятся не в корне
    * в папке с тестами есть ДО-определенные настройки eslint
    они добавляют новые правила(разрешения), кроме тех, что опсианы в коренвом конфиге
    ВАЖНО: так как теперь мы используем в качестве основы конфиг из корня, то нам НЕЛЬЗЯ указывать extends:loris
    если он будет указан, то не будут использованы опции из корня проекта(например что мы разрешили использовать в коде console.log)
    * также я убрал из eslint-конфига в тестах те опции, которые и так описаны в коренвом конфиге - незачем их дублировать, если они и так будут переиспользованы
    * конфиг jscs был удален из папки с тестами, и добавлен в корневую папку
    для тестов мы не переопределяли никаких правил, поэтому держать два конфига - не имеет смысла
    
    ** кроме того, что конфиги могут лежать в папках и наследоваться (от конфига в текущей папке, и до всех родительских папок), можно еще определять настройки на уровне файда, либо даже отдельных строк в файле
    такой пример был показан мною в файле karma.conf.js

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: 'loris/es5',
+    root: true,
+    env: {
+        browser: true
+    },
+    rules: {
+        'no-console': 0
+    }
+};
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
-    extends: 'loris/es5',
-    root: true,
     env: {
-        browser: true,
         mocha: true
-    },
-    rules: {
-        'no-console': 0
     },
     globals: {
         expect: false
