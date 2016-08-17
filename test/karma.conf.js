dca0fe1b1b4e92a1c186086a1b861f590f2e89be
dolgov-andrey <cdolgovc@gmail.com>
2016-08-17 23:07:35 +0300

    пренесены в новую директорию test

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: '../),
 
 
     // frameworks to use
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'ballHomeWork.js',
-      'ballHomeWorkTest.js'
+      '/ballHomeWork.js',
+      '/ballHomeWorkTest.js'
     ],
 
 
