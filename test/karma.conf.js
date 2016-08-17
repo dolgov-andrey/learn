dc4acc2e84bebbddd4112419d5e57d9119d1ef12
Gvozd <gvozdev.viktor@gmail.com>
2016-08-17 23:59:25 +0300

    теперь, когда файл не перегружен сообщениями об ошибках - webstorm подсветил некорректный путь к файлу, и это стало заметным именно поэтому важно сразу же исправлять все ошибки eslint/jscs, чтобы новые ошибки/варнинги были заметными либо принять решение отключить проверки в конкретном файле, как я сделал в предыдущем коммите
    все, етперь тесты запускаются

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       '../workSpace/ballHomeWork/ballHomeWork.js',
-      '/ballHomeWorkTest/ballHomeWorkTest.js'
+      './ballHomeWorkTest/ballHomeWorkTest.js'
     ],
 
 
