1f0bcaf5d36be9cfa89ea7c8c05fd890b52458e5
dolgov-andrey <cdolgovc@gmail.com>
2016-08-17 23:06:39 +0300

    удалены

--- ballHomeWorkTest.js
+++
@@ -1,7 +0,0 @@
-/* global getPositionY*/
-describe('getPositionY', function () {
-    'use strict';
-    it('тест на начальные условия по оси 0Y: нач. скорость 0, положение Yo = 0', function () {
-        expect(getPositionY(0, 0, 100)).to.equal(0, 'лежачий мяч остается лежать неподвижно');
-    });
-});
