2f3a6d9b56cfc384db24b8ee60ae256bd4dd877b
dolgov-andrey <cdolgovc@gmail.com>
2016-08-20 16:42:30 +0300

    * сделаны тесты для осей OY и OX * ведется отладка запуска тестов

--- test/ballHomeWorkTest/ballHomeWorkTest.js
+++ test/ballHomeWorkTest/ballHomeWorkTest.js
@@ -18,14 +18,65 @@ describe('getPositionY', function () {
 describe('getPositionY', function () {
     'use strict';
     it('тест броска мячика с высоты Yo = 0, startSpeedY =  Vo', function () {
-       expect(getPositionY(0, 8, 3)).to.above(0,',бросок мячика с высоты 0');
+        expect(getPositionY(0, 8, 3)).to.above(0, 'бросок мячика с высоты 0');
     });
 });
 
 /*...*/
-describe('getPosition', function () {
+describe('getPositionY', function () {
     'use strict';
     it('тест броска мячика с высоты Yo = 0, startSpeedY =  - Vo', function () {
-       expect(getPositionY(0, -8, 3)).to.above(0, 'мячик бросают в низ и он отскакивает')
+        expect(getPositionY(0, -8, 3)).to.above(0, 'мячик отскакивает при броске в низ из нулевой точки');
     });
 });
+
+/*...*/
+describe('getPositionY', function () {
+    'use strict';
+    it('тест броска мячика с высоты Yo != 0, и - Vo', function () {
+        expect(getPositionY(7, -8, 3)).to.above(0, ' мячик отскакивает от земли при броске с произвольной высоты');
+    });
+});
+
+/*...*/
+describe('getPositionY', function () {
+    'use strict';
+    it('тест на положительную скорость при 0Y = 0', function () {
+        expect(getPositionY(0, 15, 8)).to.above(0, 'мяч летит из нулевой точки');
+    });
+});
+
+/*...*/
+describe('getPositionY', function () {
+    'use strict';
+    it('бросок мячика из произвольной точки', function () {
+        expect(getPositionY(7, 18, 9)).to.above(0, 'мяч вылетает из произвольной точки 0Y');
+    });
+});
+
+
+
+/*...*/
+describe('getPositionX', function () {
+    'use strict';
+    it('тетс на начальные условия по оси 0Х: нач. скорость 0. положение Xo = 0', function () {
+        expect(getPositionX(0, 0, 100)).to.equal(0, 'мячик движется вертикально');
+    });
+});
+
+/*...*/
+describe('getPositionX', function () {
+    'use strict';
+    it('тест на положительную скорость при 0X = 0', function () {
+        expect(getPositionX(0, 15, 8)).to.above(0, 'мяч летит из нулевой точки');
+    });
+});
+
+/*...*/
+describe('getPositionX', function () {
+    'use strict';
+    it('бросок мячика из произвольной точки', function () {
+        expect(getPositionX(7, 18, 9)).to.above(0, 'мяч вылетает из произвольной точки 0Y');
+    });
+});
+
